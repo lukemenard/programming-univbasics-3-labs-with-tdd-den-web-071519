99551f1223ea87b6cbb247437808deb571207c7f
Luke Menard <lukemenard@gmail.com>
2019-07-02 19:34:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
 first_number = 12
 second_number = 4
 sum = first_number + second_number
+differnce = first_number - second_number
+product = first_number ** second_number
+quotient = first_number / second_number
